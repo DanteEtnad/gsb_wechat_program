262f207de5ae7def5aef7bddc7ec059374f08a5e
DanteEtnad <2929336605@qq.com>
2022-11-01 10:29:23 +0800

    增加了分享功能

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ import App from './App'
 
 Vue.config.productionTip = false
 
+import share from '@/common/share.js'
+
+Vue.mixin(share)
+
+
 import uView from '@/uni_modules/uview-ui'
 Vue.use(uView)
 
@@ -12,6 +17,7 @@ Vue.prototype.dayjs = dayjs
 App.mpType = 'app'
 
 const app = new Vue({
-	...App
+	...App,
+	   share
 })
 app.$mount()
